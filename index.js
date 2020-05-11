e8c10d6f4251c57cba5d27a4b1d00496088451a7
Md Abul Kashem <56566230+mdkashem@users.noreply.github.com>
2020-05-11 13:37:55 -0700

    update app

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+var express = require('express')
+var path = require('path')
+var PORT = process.env.PORT || 5000
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
